1a80322df9f563c56c46b9aa38e91962bce44758
Nguyễn Tấn Lộc <107196991+dranox@users.noreply.github.com>
2023-12-29 19:33:37 +0700

    Add files via upload

---
+++ FOL_Knowlegde_Base.py
@@ -0,0 +1,49 @@
+from Expression import *
+
+
+class FolKB:
+
+    def __init__(self, clauses=None):
+        self.clauses = []  # inefficient: no indexing
+        if clauses:
+            for clause in clauses:
+                self.learn(clause)
+
+    def learn(self, sentence):
+        if is_definite_clause(sentence):
+            self.clauses.append(sentence)
+        else:
+            raise Exception('Not a definite clause: {}'.format(sentence))
+
+    def ask_generator(self, query):
+        return fol_bc_ask(self, query)
+
+    def retract(self, sentence):
+        self.clauses.remove(sentence)
+
+    def fetch_rules_for_goal(self, goal):
+        return self.clauses
+
+
+def fol_bc_ask(kb, query):
+    answer = fol_bc_or(kb, query, {})
+    return answer
+
+
+def fol_bc_or(kb, goal, theta):
+    for rule in kb.fetch_rules_for_goal(goal):
+        lhs, rhs = parse_definite_clause(standardize_variables(rule))
+        for theta1 in fol_bc_and(kb, lhs, unify(rhs, goal, theta)):
+            yield theta1
+
+
+def fol_bc_and(kb, goals, theta):
+    if theta is None:
+        pass
+    elif not goals:
+        yield theta
+    else:
+        first, rest = goals[0], goals[1:]
+        for theta1 in fol_bc_or(kb, subst(theta, first), theta):
+            for theta2 in fol_bc_and(kb, rest, theta1):
+                yield theta2
